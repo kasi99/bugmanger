ec0b0e034daa476d7a8e12e8a39cafd951cb8412
kasi <ht333333@sina.com>
2020-04-09 06:13:37 +0800

    初始化

---
+++ bugmanger-web/src/main/java/com/kasi/bugmangerweb/BugmangerWebApplication.java
@@ -0,0 +1,13 @@
+package com.kasi.bugmangerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugmangerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugmangerWebApplication.class, args);
+    }
+
+}
---
+++ bugmanger-web/src/test/java/com/kasi/bugmangerweb/BugmangerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasi.bugmangerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugmangerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
